de1a509731325628933bfe1c85fa63bd1f9b7b30
Andrei Costescu <andrei@costescu.no>
2020-11-16 17:10:15 +0100

    Systemd timer, track exit code in run-all

---
+++ install-autoupdate.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+    echo "Run as root"
+    exit 1
+fi
+
+unit_name="docker-auto-update"
+systemd_path="/etc/systemd/system"
+
+cat <<EOF > "$systemd_path/$unit_name.timer"
+[Unit]
+Description=Docker auto update timer
+
+[Timer]
+Persistent=yes
+OnCalendar=*-*-* 05:00:00
+
+[Install]
+WantedBy=timers.target
+EOF
+
+cat <<EOF > "$systemd_path/$unit_name.service"
+[Unit]
+Description=Update all external Docker containers
+After=network.target docker.service
+Requires=network.target docker.service
+
+[Service]
+Type=oneshot
+WorkingDirectory=$(pwd -P)
+Environment=BUILD=0
+Environment=PULL=1
+ExecStart=/usr/bin/bash run-all.sh
+EOF
+
+systemctl daemon-reload
+systemctl cat "$unit_name".{timer,service}
--- run-all.sh
+++ run-all.sh
@@ -6,7 +6,7 @@ if [[ ! -f env.sh ]]; then
 fi
 
 # shellcheck disable=SC1091
-source env.sh
+source env.sh || exit 1
 
 base_dir="$(pwd -P)"
 BUILD=${BUILD:-1}
@@ -25,23 +25,36 @@ declare -A containers=(
     ["vpn"]="vpn firefox sonarr"
 )
 
+ex_code=0
 # Disable word-splitting warning, it's what we want
 # shellcheck disable=SC2086
 for name in "${!containers[@]}"; do
     svc_path="$base_dir/$name"
     if [[ ! -f "$svc_path/docker-compose.yml" ]]; then
         echo "$name no compose file"
+        ex_code=2
         continue
     fi
-    cd "$svc_path" || continue
+    if ! cd "$svc_path"; then
+        ex_code=2
+        continue
+    fi
     if ! grep -qP '^\s+build:.*' docker-compose.yml; then
-        [[ $PULL -eq 1 ]] && docker-compose pull ${containers[$name]}
+        if [[ $PULL -eq 1 ]]; then
+            docker-compose pull ${containers[$name]}
+            ex_code=$(( $? > ex_code ? $? : ex_code ))
+        fi
     elif [[ $BUILD -eq 1 ]]; then
         if [[ $PULL -eq 1 ]]; then
             docker-compose build --pull ${containers[$name]}
+            ex_code=$(( $? > ex_code ? $? : ex_code ))
         else
             docker-compose build ${containers[$name]}
+            ex_code=$(( $? > ex_code ? $? : ex_code ))
         fi
     fi
     docker-compose up -d ${containers[$name]}
+    ex_code=$(( $? > ex_code ? $? : ex_code ))
 done
+
+exit $ex_code
